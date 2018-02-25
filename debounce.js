1dcc430f42d4f5830c7332b8a488ca94e6e92663
coderhu <826778226@qq.com>
2018-02-25 17:18:27 +0800

    Create debounce.js
    函数防抖实现

---
+++ debounce.js
@@ -0,0 +1,11 @@
+function debounce(func, delay){
+		var timer;
+    
+		return function(){
+			var self = this;
+			var args = arguments;
+			
+			window.clearTimeout(timer);
+			timer = window.setTimeout(function(){func.apply(self, args)},delay);
+		}
+	}
